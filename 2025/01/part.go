d8365411725dd01f6d5f518e36e5b528b3ecd1a6
aBerglehner <alexanderberglehner2@gmail.com>
2026-01-05 02:14:32 +0100

    feat: 01

---
+++ 2025/01/part.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/alex/flipflop/utils"
+)
+
+func main() {
+	part3()
+	part3Para()
+}
+
+func part1() {
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filename)
+
+	inputPath := filepath.Join(baseDir, "input")
+	data, err := utils.ReadLines(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ans := 0
+	for _, line := range data {
+		a := strings.Count(line, "ba")
+		b := strings.Count(line, "na")
+		c := strings.Count(line, "ne")
+		ans += a + b + c
+
+		// fmt.Println(line)
+		// fmt.Printf("a: %v\n", a)
+		// fmt.Printf("b: %v\n", b)
+		// fmt.Println()
+	}
+	fmt.Printf("ans: %v\n", ans)
+}
+
+func part2() {
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filename)
+
+	inputPath := filepath.Join(baseDir, "input")
+	data, err := utils.ReadLines(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ans := 0
+	for _, line := range data {
+		a := strings.Count(line, "ba")
+		b := strings.Count(line, "na")
+		c := strings.Count(line, "ne")
+		sum := a + b + c
+		if sum%2 == 0 {
+			ans += sum
+		}
+
+		// fmt.Println(line)
+		// fmt.Printf("a: %v\n", a)
+		// fmt.Printf("b: %v\n", b)
+		// fmt.Println()
+	}
+	fmt.Printf("ans: %v\n", ans)
+}
+
+func part3() {
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filename)
+
+	inputPath := filepath.Join(baseDir, "input")
+	data, err := utils.ReadLines(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ans := 0
+	for _, line := range data {
+		a := strings.Count(line, "ba")
+		b := strings.Count(line, "na")
+		c := strings.Count(line, "ne")
+		sum := a + b + c
+		if c == 0 {
+			ans += sum
+		}
+
+		// fmt.Println(line)
+		// fmt.Printf("a: %v\n", a)
+		// fmt.Printf("b: %v\n", b)
+		// fmt.Println()
+	}
+	fmt.Printf("ans: %v\n", ans)
+}
+
+func part3Para() {
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filename)
+
+	inputPath := filepath.Join(baseDir, "input")
+	data, err := utils.ReadLines(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	splits := runtime.NumCPU()
+	tasks := utils.SplitTasks(data, splits)
+	var wg sync.WaitGroup
+	ch := make(chan int, len(tasks))
+	for _, data := range tasks {
+		wg.Add(1)
+		go taskPara(data, ch, &wg)
+	}
+
+	wg.Wait()
+	close(ch)
+
+	ans := 0
+	for v := range ch {
+		ans += v
+	}
+	fmt.Printf("ans: %v\n", ans)
+}
+
+func taskPara(data []string, ch chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	ans := 0
+	for _, line := range data {
+		a := strings.Count(line, "ba")
+		b := strings.Count(line, "na")
+		c := strings.Count(line, "ne")
+		sum := a + b + c
+		if c == 0 {
+			ans += sum
+		}
+	}
+	ch <- ans
+}
